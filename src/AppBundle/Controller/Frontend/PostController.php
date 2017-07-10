5374a55658749d88a2138d715b05c3ab96e78e61
David Lima <dave.lima@live.com>
2017-07-10 08:26:06 -0300

    Implementing Preview feature on Posts module

--- src/AppBundle/Controller/Frontend/PostController.php
+++ src/AppBundle/Controller/Frontend/PostController.php
@@ -65,7 +65,64 @@ class PostController extends Controller
             'formattedPubdate' => $formattedPubdate,
             'pageTitle' => $post->getTitle(),
             'tags' => $tags,
-            'menus' => $menus->getSiteMenu()
+            'menus' => $menus->getSiteMenu(),
+            'preview' => false
+        ]);
+    }
+
+    /**
+     * @Route("/preview/{section}/{slug}/", name="posts-preview")
+     */
+    public function previewAction(Request $request, $section, $slug)
+    {
+        if (! $this->getUser()) {
+            throw $this->createAccessDeniedException('Access denied');
+        }
+
+        $menus = $this->container->get('utils.menus');
+
+        $postRepository = $this->getDoctrine()->getRepository('AppBundle:Post');
+
+        $post = $postRepository->createQueryBuilder('p');
+
+        $post->where('p.slug = :slug')
+            ->setParameters([
+                'slug' => $slug
+            ]);
+
+        $post->setMaxResults(1);
+        $post = $post->getQuery()->execute();
+
+        if ($post) {
+            $post = $post[0];
+        }
+
+        if (! $post || $post && $post->getSection()->getSlug() != $section) {
+            throw $this->createNotFoundException('Publicação não encontrada');
+        }
+
+        setlocale(LC_TIME, 'pt_BR.UTF8');
+
+        $pubDate = explode('-', $post->getPubdate()->format('Y-m-d'));
+        $formattedPubdate = strftime('%d de %B de %G', mktime(0, 0, 0, $pubDate[1], $pubDate[2], $pubDate[0]));
+
+        $tags = [];
+
+        if ($post->getTags()) {
+            foreach ($post->getTags() as $tag) {
+                $tags[] = $tag->getId();
+            }
+        }
+
+        return $this->render('frontend/post/read.html.twig', [
+            'section' => $section,
+            'slug' => $slug,
+            'post' => $post,
+            'formattedPubdate' => $formattedPubdate,
+            'pageTitle' => 'PREVIEW | ' . $post->getTitle(),
+            'tags' => $tags,
+            'menus' => $menus->getSiteMenu(),
+            'preview' => true
         ]);
     }
 }
